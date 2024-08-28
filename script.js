0f12008517da49138ae86c31e971512e781135b5
Cerr1738 <ezechisom966@gmail.com>
2024-08-28 12:22:53 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('burger-menu').addEventListener('click', function() {
+    document.getElementById('header-list').classList.toggle('active');
+    document.getElementById('header-button').classList.toggle('active');
+});
